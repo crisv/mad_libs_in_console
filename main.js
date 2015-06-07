a4e5c8fe0441cb32276d208d7f830964b1287531
crisv <cris.visan@krujex.com>
2015-06-06 20:45:58 -0700

    added popup box

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 alert("I'm working with JavaScript");
+
+var parent_unit = prompt('What parent loves you the least??')
+
+function mad_libs() {
+  var nouns = ["computer","milk","gun","shotgun"];
+
+document.write('My '+parent_unit+' left home when I was 3, He didnt much for my Ma and me, cept this old '+nouns[0]+' and an empty bottle of '+nouns[1]);
+}
+
+mad_libs();
+
